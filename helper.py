8fff2e55136b742c3f5f1ef058a45be266fae14c
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-10-20 23:37:52 -0400

    Fix formatting of 'helper' to follow PEP8 (and other changes)

--- helper.py
+++ helper.py
@@ -24,32 +24,41 @@ async def spam_test():
 
         await asyncio.sleep(60*spam_message_min)
 
+
 @client.event
 async def on_message(message):
     if message.content.startswith('$help'):
-        msg = client.send_message(channel, 'Alright, <@'+ str(message.author.id) +'>! I PMed you some help!')
-        msg = await client.send_message(message.author, 'Fetching Help Library...')
+        await client.send_message(channel, ('Alright, <@' +
+                                  str(message.author.id) +
+                                  '>! I PMed you some help!'))
+        await client.send_message(message.author,
+                                  'Fetching Help Library...')
         await asyncio.sleep(3)
         await client.edit_message(msg, 'Help Text Coming Soon')
+
 
 @client.event
 async def on_message(message):
     """
         Command List:
-        [*] "$guess": Allows user to guess a "randomly" generated 1 to 10 digit.
-        """
+        [*] "$guess": Allows user to guess a "randomly" generated 1 to 10
+            digit.
+    """
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-    
+
     if ANNOYING:
-        msg = 'Howdy <@' + str(message.author.id) + '>. You said: "{0}". Here is your email: \n{1}'.format(message.content, str(message.author.avatar_url))
+        msg = ('Howdy <@{}>. You said: "{}". Here is your email: \n{}'
+               .format(str(message.author.id), message.content,
+                       str(message.author.avatar_url)))
         mes = await client.send_message(message.channel, msg)
-    
+
     # GUESSING GAME ("$guess")
     if message.content.startswith('$guess'):
-        await client.send_message(message.channel, 'Hey, <@'+ str(message.author.id) +'>, Guess a number between 1 to 10 (Answer within 5 seconds)')
-        
+        await client.send_message(message.channel, ('Guess a number between' +
+                                  ' 1 to 10 (Answer within 5 seconds)'))
+
         def guess_check(m):
             m = m.content
             start_string = '<@{}>'.format(client.user.id)
@@ -59,17 +68,24 @@ async def on_message(message):
                 m = m[len(start_string):]
                 m.strip(' ')
                 return m.isdigit()
-        
-        guess = await client.wait_for_message(timeout=5.0, author=message.author, check=guess_check)
+
+        guess = await client.wait_for_message(timeout=5.0,
+                                              author=message.author
+                                              check=guess_check)
         answer = random.randint(1, 10)
         if guess is None:
-            fmt = 'Sorry <@' + str(message.author.id) + '> , you took too long. It was {}.'
-            await client.send_message(message.channel, fmt.format(answer))
+            fmt = ('Sorry <@{}> , you took too long. It was {}.'
+                   .format(str(message.author.id), answer))
+            await client.send_message(message.channel, fmt)
             return
         if int(guess.content) == answer:
-            await client.send_message(message.channel, 'You are right, <@' + str(message.author.id) + '>!')
+            fmt = ('You are right, <@{}>!').format(str(message.author.id))
+            await client.send_message(message.channel, fmt)
         else:
-            await client.send_message(message.channel, 'Sorry, <@' + str(message.author.id) + '>. It is actually {}.'.format(answer))
+            fmt = ('Sorry, <@{}>. It is actually {}.'
+                   .format(str(message.author.id), answer))
+            await client.send_message(message.channel, fmt)
+
 
 @client.event
 async def on_ready():
